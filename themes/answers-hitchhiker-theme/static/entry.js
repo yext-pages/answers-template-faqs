a23bc34b1acb80a291d227f503ebcc71350fffd3
pages-system <55206003+pages-system@users.noreply.github.com>
2020-09-23 21:43:46 -0400

    Initial commit

---
+++ themes/answers-hitchhiker-theme/static/entry.js
@@ -0,0 +1,11 @@
+// Import global polyfills
+import 'core-js/stable';
+
+// Import all SCSS
+import Scss from './scss/answers/_default.scss';
+
+// Import all JS assets
+import Formatters from './js/formatters';
+export { Formatters };
+export { getDefaultMapApiKey } from './js/default-map-api-key';
+export { isStaging } from './js/is-staging';
